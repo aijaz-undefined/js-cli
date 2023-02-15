a5e23fdf4ae4fea22054e0d7610fcd4098d8e35a
Aijaz Suri <suriaijaz@gmail.com>
2023-02-15 19:59:58 +0530

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,14 +1,14 @@
 var data = {
   summary:
-    "Hello!! I am Neha from India. I am working as front-end developer. I have 8+ years of experience in the UI domain. Techstack I am working on is - HTML5, CSS3, JavaScript, Reactjs, Nodejs , and I know UI designing tools too. I am active speaker and writer too.",
+    "Hello!! I am Aijaz from India. I am working as front-end developer. I have 10+ years of experience in the UI domain. Techstack I am working on is - HTML5, CSS3, JavaScript, Reactjs, Nodejs , and I know UI designing tools too.",
   aboutme:
-    "I am Neha from India. I am working as front-end developer. I have 8+ years of experience in the UI domain. Techstack I am working on is - HTML5, CSS3, JavaScript, Reactjs, Nodejs , and I know UI designing tools too. I am active speaker and writer too.",
+    "I am Aijaz from India. I am working as front-end developer. I have 10+ years of experience in the UI domain. Techstack I am working on is - HTML5, CSS3, JavaScript, Reactjs, Nodejs , and I know UI designing tools too.",
   contactme:
-    "You can email me at nsharma215@gmail.com\nTwitter : twitter.com/hellonehha\nLinkdin : linkdin.co./nehhasharma\n",
+    "You can email me at suriaijaz@gmail.com\nTwitter : https://twitter.com/AijazSuri\nLinkdin : https://www.linkedin.com/in/aijazsuri/\n",
   experience:
-    "I have 8+ years of experience in front-end domain. Currently I am working as Technical Lead at HCL Products and Platforms. I am working with the team to develop and implement their new design system.",
+    "I have 10+ years of experience in front-end domain. Currently I am working as Technical Lead at Mavenir Systems. I am working with the team to develop and implement their new design system.",
   techstack:
-    "For front-end tooling  I am working on HTML5\nCSS3\nJavascript\nReactjs\nRedux\nNodejs. I know little bit of UI designing too and for that I use Photoshop and Sketch."
+    "For front-end tooling  I am working on HTML5\nCSS3\nJavascript\nReactjs\nRedux\nNodejs. I know little bit of UX designing too and for that I use Photoshop and Sketch."
 };
 
 module.exports = data;
